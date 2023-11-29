651fc196b3866d762c6d92aabecc5776b1d5a65f
DorVaknin <dor.vkn@gmail.com>
2023-11-29 18:18:58 +0700

    updated gameController to use services

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,33 +1,19 @@
-const Game = require('../models/gameModel');
-
+const GameService = require('../services/gameService');
 exports.startNewGame = async (req, res) => {
-  try {
-    const frames = new Array(10).fill({ rolls: [], score: 0 });
-
-    const newGame = new Game({
-      playerName: req.body.playerName,
-      frames: frames,
-      currentScore: 0,
-      completed: false
-    });
-
-    await newGame.save();
-
-    res.status(201).json({
-      message: 'New game started successfully',
-      gameId: newGame._id,
-      data: newGame
-    });
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
-
+    try {
+      const newGame = await GameService.startNewGame(req.body.playerName);
+      res.status(201).json(newGame);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  };
 
 exports.recordRoll = async (req, res) => {
-  try {
-    res.json({ message: 'Roll recorded' });
-  } catch (err) {
+try {
+    const { gameId, pins } = req.body;
+    const updatedGame = await GameService.recordRoll(gameId, pins);
+    res.json(updatedGame);
+} catch (err) {
     res.status(500).send(err.message);
-  }
+}
 };
