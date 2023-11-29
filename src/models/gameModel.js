187e4cf422c6bf399f4f95fbe95014994c7eee9f
DorVaknin <dor.vkn@gmail.com>
2023-11-29 18:18:11 +0700

    updated gameModel

--- src/models/gameModel.js
+++ src/models/gameModel.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
 const frameSchema = new mongoose.Schema({
-  frameNumber: Number,
   rolls: [Number],
-  score: Number
+  isTenthFrame: { type: Boolean, default: false }
 });
 
 const gameSchema = new mongoose.Schema({
-  playerName: { type: String, required: true },
-  frames: [frameSchema],
-  currentScore: { type: Number, default: 0 },
-  completed: { type: Boolean, default: false }
-});
+    playerName: { type: String, required: true },
+    frames: {
+      type: [frameSchema],
+      default: () => new Array(10).fill({ rolls: [], isTenthFrame: false })
+    },
+    currentScore: { type: Number, default: 0 },
+    completed: { type: Boolean, default: false }
+  });
 
 module.exports = mongoose.model('Game', gameSchema);
