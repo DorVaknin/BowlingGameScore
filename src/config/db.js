2ac4f881108909ebf54ad28c98b425a00c1a6368
DorVaknin <dor.vkn@gmail.com>
2023-11-30 08:57:07 +0700

    changed connection string functionality

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,24 @@ const mongoose = require('mongoose');
 const { log } = require('../utils/helpers')
 const CONNECTION_STRING = 'mongodb+srv://dorvaknin:6WYWBhNDY06ZrdKr@bowlingscorecluster.5amsl3d.mongodb.net/BowlingScoresDB';
 const connect = async () => {
-
-  if (process.env.NODE_ENV === 'test') {
-    connectionString = process.env.TEST_MONGODB_URI;
-  } else {
-    connectionString = CONNECTION_STRING;
-  }
-
   try {
-    await mongoose.connect(connectionString);
+    await mongoose.connect(getConnectionString());
     log('Connected to MongoDB...');
   } catch (err) {
     console.error('Could not connect to MongoDB...', err);
     process.exit(1);
   }
 };
+
+const getConnectionString = () => {
+  let connectionString;
+  if (process.env.NODE_ENV === 'test') {
+    connectionString = process.env.TEST_MONGODB_URI;
+  } else {
+    connectionString = CONNECTION_STRING;
+  }
+
+  return connectionString;
+}
 
 module.exports = { connect };
